1977956551224691098114aae37193ae4a95334a
EEFGJPV-fam <grupopiifam2024@gmail.com>
2025-05-26 16:04:58 -0300

    CSS

---
+++ js/menuScroll.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('nav a, button a').click(function(){
+		var href = $(this).attr('href');
+		var offSetTop = $(href).offset().top;
+
+		$('html,body').animate({'scrollTop':offSetTop});
+
+		return false;
+	})
+})
